4f042e79db36a15631440e920ec720f8f7678bc1
mcc <60859013+AlexisBal@users.noreply.github.com>
2020-12-05 01:58:32 +0100

    master

--- screens/RallyeQ1.tsx
+++ screens/RallyeQ1.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Text, View } from '../components/Themed';
 import { StyleSheet, Image, ScrollView} from 'react-native';
-import { Button, Overlay } from 'react-native-elements';
+import { Button, colors } from 'react-native-elements';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../types';
 import Constants from 'expo-constants';
@@ -10,35 +10,161 @@ type Props = StackScreenProps<RootStackParamList, 'RallyeQ1'>;
 
 export class RallyeQ1 extends React.Component<Props> {
   constructor(props: Props) {
-    super(props)
+    super(props);
+    this.state = {
+      backgroundColor1: '#2196F3',
+      backgroundColor2: '#2196F3',
+      backgroundColor3: '#2196F3',
+      backgroundColor4: '#2196F3',
+      nombre_reponses: 0,
+      rallyes_reponse1: '',
+      rallyes_reponse2: '',
+      rallyes_reponse3: '',
+      rallyes_reponse4: '',
+      display: 'none'
+    };
+    this.scroll = null;
+    this.ref = React.createRef();
+    this.ChangeColor1 = this.ChangeColor1.bind(this);
+    this.ChangeColor2 = this.ChangeColor2.bind(this);
+    this.ChangeColor3 = this.ChangeColor3.bind(this);
+    this.ChangeColor4 = this.ChangeColor4.bind(this);
   }
+
+  ChangeColor1(rep){
+      if (this.state.backgroundColor1 == '#2196F3') {
+        if (this.state.nombre_reponses < this.props.route.params.rallye.rallye.question1.point) {
+          var reponses = this.state.nombre_reponses + 1
+          if (reponses == this.props.route.params.rallye.rallye.question1.point) {
+            this.setState({
+              nombre_reponses: reponses,
+              backgroundColor1: 'red',
+              rallyes_reponse1: rep,
+              display: 'true'
+            })
+          }
+          else {
+            this.setState({
+              nombre_reponses: reponses,
+              backgroundColor1: 'red',
+              rallyes_reponse1: rep
+            }) 
+          }
+        }
+      }
+      else {
+        var reponses = this.state.nombre_reponses - 1
+        this.setState({
+          backgroundColor1: '#2196F3',
+          nombre_reponses: reponses,
+          rallyes_reponse1: '',
+          display: 'none'
+        }) 
+      }
+  }
+
+  ChangeColor2(rep){
+      if (this.state.backgroundColor2 == '#2196F3') {
+        if (this.state.nombre_reponses < this.props.route.params.rallye.rallye.question1.point) {
+          var reponses = this.state.nombre_reponses + 1
+          if (reponses == this.props.route.params.rallye.rallye.question1.point) {
+            this.setState({
+              nombre_reponses: reponses,
+              backgroundColor2: 'red',
+              rallyes_reponse2: rep,
+              display: 'true'
+            }) 
+          }
+          else {
+            this.setState({
+              nombre_reponses: reponses,
+              backgroundColor2: 'red',
+              rallyes_reponse2: rep,
+            }) 
+          }
+        }
+      }
+      else {
+        var reponses = this.state.nombre_reponses - 1
+        this.setState({
+          backgroundColor2: '#2196F3',
+          nombre_reponses: reponses,
+          rallyes_reponse2: "",
+          display: 'none'
+        }) 
+      }
+  }
+
+  ChangeColor3(rep){
+      if (this.state.backgroundColor3 == '#2196F3') {
+        if (this.state.nombre_reponses < this.props.route.params.rallye.rallye.question1.point) {
+          var reponses = this.state.nombre_reponses + 1
+          if (reponses == this.props.route.params.rallye.rallye.question1.point) {
+            this.setState({
+              nombre_reponses: reponses,
+              backgroundColor3: 'red',
+              rallyes_reponse3: rep,
+              display: 'true'
+            })
+          }
+          else {
+            this.setState({
+              nombre_reponses: reponses,
+              backgroundColor3: 'red',
+              rallyes_reponse3: rep
+            }) 
+          }
+        }
+      }
+      else {
+        var reponses = this.state.nombre_reponses - 1
+        this.setState({
+          backgroundColor3: '#2196F3',
+          nombre_reponses: reponses,
+          rallyes_reponse3: "",
+          display: 'none'
+        }) 
+      }
+  }
+
+  ChangeColor4(rep){
+    if (this.state.backgroundColor4 == '#2196F3') {
+      if (this.state.nombre_reponses < this.props.route.params.rallye.rallye.question1.point) {
+        var reponses = this.state.nombre_reponses + 1
+        if (reponses == this.props.route.params.rallye.rallye.question1.point) {
+            this.setState({
+              nombre_reponses: reponses,
+              backgroundColor4: 'red',
+              rallyes_reponse4: rep,
+              display: 'true'
+            }) 
+          }
+          else {
+            this.setState({
+              nombre_reponses: reponses,
+              backgroundColor4: 'red',
+              rallyes_reponse4: rep
+            }) 
+          }
+      }
+    }
+    else {
+      var reponses = this.state.nombre_reponses - 1
+      this.setState({
+        backgroundColor4: '#2196F3',
+        nombre_reponses: reponses,
+        rallyes_reponse4: "",
+        display: 'none'
+      }) 
+    }
+  }
+
   render() {
     const id_parcours = this.props.route.params.id_parcours;
     const rallye = this.props.route.params.rallye;
-    var rallyes_reponse = {
-      Q1: '',
-      Q2: '',
-      Q3: '',
-      Q4: '',
-      Q5: '',
-      Q6: '',
-      Q7: '',
-      Q8: '',
-      Q9: '',
-      Q10: '',
-      Q11: '',
-      Q12: '',
-      Q13: '',
-      Q14: '',
-      Q16: '',
-      Q17: '',
-      Q18: '',
-      Q19: '',
-      Q20: ''
-    };
     return (
       <View style={styles.main_container}>
-        <ScrollView>
+        <ScrollView ref={this.ref} onContentSizeChange={() => this.ref.current.scrollToEnd({ animated: true })}>
           <Image
             style={styles.image}
             source={rallye.rallye.question1.photo}
@@ -49,29 +175,30 @@ export class RallyeQ1 extends React.Component<Props> {
           </Text>
           <View style={styles.container}>
             <View style={styles.button}>
-                <Button buttonStyle={{borderRadius: 20, height: 45}} containerStyle={{borderRadius: 20, flex:1}} title={rallye.rallye.question1.reponse1} onPress={() => {
-                    rallyes_reponse.Q1 = 'A';
-                    this.props.navigation.navigate("RallyeQ2", { rallye, id_parcours, rallyes_reponse });
+                <Button buttonStyle={{borderRadius: 20, height: 45, backgroundColor: this.state.backgroundColor1}} containerStyle={{borderRadius: 20, flex:1}} title={rallye.rallye.question1.reponse1} onPress={() => {
+                    this.ChangeColor1('A')
                 }}/>
             </View>
             <View style={styles.button}>
-                <Button buttonStyle={{borderRadius: 20, height: 45 }}  containerStyle={{borderRadius: 20, flex:1}} title={rallye.rallye.question1.reponse2} onPress={() => {
-                    rallyes_reponse.Q1 = 'B';
-                    this.props.navigation.navigate("RallyeQ2", { rallye, id_parcours, rallyes_reponse });
+                <Button buttonStyle={{borderRadius: 20, height: 45, backgroundColor: this.state.backgroundColor2}}  containerStyle={{borderRadius: 20, flex:1}} title={rallye.rallye.question1.reponse2} onPress={() => {
+                    this.ChangeColor2('B')
                 }}/>
             </View>
             <View style={styles.button}>
-                <Button buttonStyle={{borderRadius: 20, height: 45 }}  containerStyle={{borderRadius: 20, flex:1}} title={rallye.rallye.question1.reponse3} onPress={() => {
-                    rallyes_reponse.Q1 = 'C';
-                    this.props.navigation.navigate("RallyeQ2", { rallye, id_parcours, rallyes_reponse });
+                <Button buttonStyle={{borderRadius: 20, height: 45, backgroundColor: this.state.backgroundColor3 }}  containerStyle={{borderRadius: 20, flex:1}} title={rallye.rallye.question1.reponse3} onPress={() => {
+                    this.ChangeColor3('C')
                 }}/>
             </View>
             <View style={styles.button}>
-                <Button buttonStyle={{borderRadius: 20, height: 45 }}  containerStyle={{borderRadius: 20, flex:1}} title={rallye.rallye.question1.reponse4} onPress={() => {
-                    rallyes_reponse.Q1 = 'D';
-                    this.props.navigation.navigate("RallyeQ2", { rallye, id_parcours, rallyes_reponse });
+                <Button buttonStyle={{borderRadius: 20, height: 45, backgroundColor: this.state.backgroundColor4 }}  containerStyle={{borderRadius: 20, flex:1}} title={rallye.rallye.question1.reponse4} onPress={() => {
+                    this.ChangeColor4('D')
                 }}/>
             </View>
+          </View>
+          <View style={{flex:1, marginTop: 20, display: this.state.display}}>
+              <View style={styles.button}>
+                  <Button buttonStyle={{flex:1, height:70, backgroundColor: "green"}} containerStyle={{ flex:1}} title="CONFIRMER"  onPress={() => {}}/>
+              </View>
           </View>
         </ScrollView>
       </View>
@@ -82,9 +209,6 @@ export class RallyeQ1 extends React.Component<Props> {
 const styles = StyleSheet.create({
     main_container: {
       flex: 1,
-      paddingLeft: 20,
-      paddingRight: 20,
-      paddingBottom: Constants.statusBarHeight,
     },
     container: {
       flex:1,
@@ -102,12 +226,16 @@ const styles = StyleSheet.create({
       alignSelf: 'center'
     },
     innerText:{
+      paddingLeft: 20,
+      paddingRight: 20,
       marginTop: 15,
       fontSize: 21,
       textAlign: 'left',
       fontWeight: 'bold',
     },
     texte: {
+      paddingLeft: 20,
+      paddingRight: 20,
       marginTop: 15,
       fontSize: 21,
       textAlign: 'left',
